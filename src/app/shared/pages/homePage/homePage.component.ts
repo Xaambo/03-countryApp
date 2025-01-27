5a62a0858e88a78f64ac0538f3e1a50702536e59
Jordi Joan Bautista <jjoanbau@emeal.nttdata.com>
2025-01-27 16:52:09 +0100

    firs commit

---
+++ src/app/shared/pages/homePage/homePage.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  standalone: false,
+  templateUrl: './homePage.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomePageComponent {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/homePage/homePage.component';
+import { AboutPageComponent } from './pages/aboutPage/aboutPage.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contactPage/contactPage.component';
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
+  ],
+})
+export class SharedModule {}
